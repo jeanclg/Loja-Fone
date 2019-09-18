951462e53112269c1284cb6b132d5476eca9248c
Jean Garcia <jean_clg@hotmail.com>
2019-09-18 19:25:53 -0300

    Clique no heart e no add carrinho

--- js/description-home.js
+++ js/description-home.js
@@ -1,3 +1,16 @@
 const $heart = window.document.querySelector(".heart");
+const $addChart = window.document.querySelector(".-second");
+const $chartQtd = window.document.querySelector(".-last");
 
-console.log($heart);
+$heart.addEventListener("click", HandleClick);
+$addChart.addEventListener("click", AddChart);
+
+function HandleClick() {
+    console.log("clicou no " + $heart.className);
+}
+
+function AddChart() {
+    console.log("Funcionou!");
+
+    $chartQtd.innerHTML = "Carrinho(11)";
+}

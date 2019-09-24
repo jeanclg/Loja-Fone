f42511c170a663b70312cca87df25dc7459e5b77
Jean Garcia <jean_clg@hotmail.com>
2019-09-24 19:43:35 -0300

    Heart active

--- js/description-home.js
+++ js/description-home.js
@@ -3,5 +3,5 @@ const $heart = document.querySelector(".heart");
 $heart.addEventListener("click", HandleClick);
 
 function HandleClick() {
-    console.log("clicou no " + $heart.className);
+    $heart.classList.toggle("-active");
 }
